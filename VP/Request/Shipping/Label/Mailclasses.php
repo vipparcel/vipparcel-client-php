c52854c150eb9fa72aff079bc9b79079b331a0a4
MGerasimchuk <gerasimchuk.mihail@gmail.com>
2017-03-28 10:55:42 +0700

    add endpoint

---
+++ VP/Request/Shipping/Label/Mailclasses.php
@@ -0,0 +1,22 @@
+<?php
+
+class VP_Request_Shipping_Label_Mailclasses extends VP_Abstract_Request
+{
+    public function info_method()
+    {
+        return self::METHOD_GET;
+    }
+
+    public function info_url()
+    {
+        return '/shipping/label/mailclasses';
+    }
+
+    public function info_params()
+    {
+        return array(
+            'authToken'
+        );
+    }
+
+}
