a8dfa75f9a3d1f50e0d17e598fceee97b71014f8
MGerasimchuk <gerasimchuk.mihail@gmail.com>
2017-03-28 11:36:07 +0700

    change mailClasses url

--- VP/Request/Shipping/Label/Mailclasses.php
+++ VP/Request/Shipping/Label/Mailclasses.php
@@ -9,7 +9,7 @@ class VP_Request_Shipping_Label_Mailclasses extends VP_Abstract_Request
 
     public function info_url()
     {
-        return '/shipping/label/mailclasses';
+        return '/shipping/label/mailClasses';
     }
 
     public function info_params()
